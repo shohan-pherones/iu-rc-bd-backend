db33185b94f7f769d6cda1834da9c832c310436a
shohan-pherones <shohan.sub.56@gmail.com>
2025-01-16 03:21:08 +0600

    add interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,53 @@
+import { Document } from "mongoose";
+
+export interface Education extends Document {
+  institute: string;
+  degree: string;
+  department: string;
+  batch: string;
+  roll: string;
+}
+
+export interface User extends Document {
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  phone?: string;
+  address?: string;
+  sex: "male" | "female" | "other";
+  role: "user" | "member" | "admin";
+  photo?: string;
+  dateOfBirth?: Date;
+  hobbies?: string[];
+  education: Education;
+  comments: Comment[];
+  workshops: Workshop[];
+}
+
+export interface Instructor extends Document {
+  name: string;
+  photo?: string;
+  designation: string;
+}
+
+export interface Comment extends Document {
+  text: string;
+  user: User;
+}
+
+export interface Workshop extends Document {
+  name: string;
+  banner?: string;
+  description: string;
+  host: string;
+  deadline: Date;
+  maxAttendee: number;
+  dateTime: Date;
+  duration: number;
+  status: "upcoming" | "running" | "past";
+  instructors: Instructor[];
+  users: User[];
+  comments: Comment[];
+}
